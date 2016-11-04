76d87d328a8854917b10827ccac7159f130b0622
Zachariah Grummons <zachariah.grummons@ttu.edu>
2016-11-04 02:00:32 -0500

    Initial commit for WebService and the starting codebase for it

---
+++ WebService/CS4365 Web Service/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CS4365_Web_Service
+{
+    internal class Program
+    {
+        private static TcpListener _webListener;
+        private static TcpListener _robotListener;
+
+        private static void Main()
+        {
+            Initialize();
+
+            while (true)
+            {
+                var client = _robotListener.AcceptTcpClient();
+                var stream = client.GetStream();
+
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream);
+                string input;
+
+                while (client.Connected && (input = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+        }
+
+        private static void Initialize()
+        {
+            _webListener = new TcpListener(IPAddress.Any, 9000);
+            _robotListener = new TcpListener(IPAddress.Any, 9001);
+
+            _webListener.Start();
+            _robotListener.Start();
+        }
+    }
+}
