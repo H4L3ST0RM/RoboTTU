ec95f8f6d0b8e49707fbf540b729f3d902809f6e
John <john@localhost.localdomain>
2016-12-07 03:53:25 -0600

    Still buggy, but significantly better. Posting on here to pass, to other team members to help me trouble shoot

--- WebClient/mapping.cpp
+++ WebClient/mapping.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 create::Create* robot;
 
+//Declaring all functions
 int getState(create::Create* robot);
 void goState0(create::Create* robot);
 void goState1(create::Create* robot);
@@ -11,12 +12,16 @@ void goState2(create::Create* robot,int state[2]);
 void goState3(create::Create* robot,int state[2]);
 void writeData(std::ofstream* dataFile);
 
+//Global Variables
+int previous = 0;
+int current = 1;
+int state[2];
 
 int main() {
-  //DECLARING ROBOT
   std::cout<< "First Line Of Main\n";
   std::string port = "/dev/ttyUSB0";
   int baud = 115200;
+  //Making robot object
   create::RobotModel model = create::RobotModel::CREATE_2;
   robot = new create::Create(model);
   // Attempt to connect to Create
@@ -27,36 +32,47 @@ int main() {
       return 1;
   }
   // Note: Some functionality does not work as expected in Safe mode
-  robot->setMode(create::MODE_FULL);
-//
-  int state[2];
-  state[0] = 2;   //Previous State
-  state [1] = 2;  //Current State
+  robot->setMode(create::MODE_SAFE);
+
+  //Set current/previous states to 2
+  state[previous] = 2;
+  state[current] = 2;
+  //Create ofstream object for outputting data to file
   std::ofstream *dataFile;
-  robot->drive(0.1,0.0);
+  //Set intial direction to forward
+//  robot->drive(0.1,0.0);
 /*******************************************************************************/
-
-  while (true) {
+  //Main While Loop
+  while (!robot->isCleanButtonPressed()) {
+    robot->drive(0.1,0.0);
     std::cout<<"Indside While Loop\n";
-    std::cout<<"state[0]: " << state[0] << "\n";
-    std::cout<<"state[1]: " << state[1] << "\n";
+    usleep(1000 * 100); //10hz
+    std::cout<<"isLightBumperLeft: " << robot->isLightBumperLeft()<<"\n";
+    std::cout<<"isLightBumperFrontLeft: " << robot->isLightBumperFrontLeft()<<"\n";
+    std::cout<<"isLightBumperCenterLeft: " << robot->isLightBumperCenterLeft()<<"\n";
+    std::cout<<"isLightBumperRight: " << robot->isLightBumperRight()<<"\n";
+    std::cout<<"isLightBumperFrontRight: " << robot->isLightBumperFrontRight()<<"\n";
+    std::cout<<"isLightBumperCenterRight: " << robot->isLightBumperCenterRight()<<"\n";
 
-    state[0] = state[1];
-    state[1] = getState(robot);
-    if (state[0] != state[1]) {
-      if (state[1] == 0){
-        //writeData(dataFile);
+    //Update previous and current states
+    state[current] = getState(robot);
+    std::cout<<"state[previous]: " << state[previous] << "\n";
+    std::cout<<"state[current]: " << state[current] << "\n";
+    //If there has been a state change, we need to update robot statis
+    if (state[previous] != state[current]) {
+      //Before changing state, save data to file for mapping later
+      //writeData(dataFile);
+      //Go to whatever state is now current
+      if (state[current] == 0){
         goState0(robot);
       }
-      if (state[1] == 1 ) {
-        //writeData(dataFile);
+      if (state[current] == 1 ) {
         goState1(robot);
       }
-      if (state[1] == 2) {
+      if (state[current] == 2) {
         goState2(robot, state);
       }
-      if (state[1] == 3) {
-        //writeData(dataFile);
+      if (state[current] == 3) {
         goState3(robot, state);
       }
     }
@@ -66,80 +82,74 @@ int main() {
   delete robot;
   return 0;
 }
-
+//If current state is zero, then one or more of the 3 left light bumpers is on
 void goState0(create::Create* robot) {
   std::cout<<"Inside State0\n";
-  if (!(robot->isLightBumperCenterLeft()) && !(robot->isLightBumperFrontLeft())) {
-    robot->drive(1.0, 0.0);
-  }
-  else {
-    while ((robot->isLightBumperFrontLeft()) || (robot->isLightBumperCenterLeft())) {
-      robot->drive(0.0,1.0);
+  state[previous] = 0;
+  //if its only the far left bumper that is on, move straight
+    while ((robot->isLightBumperFrontLeft()) || (robot->isLightBumperCenterLeft())
+            || (robot->isLightBumperLeft())) {
+      //Turn right
+      robot->drive(0.0,-0.1);
     }
-    robot->drive(1.0,0.0);
-  }
+    //Drive straight
+    robot->drive(0.1,0.0);
 }
 
+//If current state is one, then one or more of the 3 right light bumpers is on
 void goState1(create::Create* robot) {
   std::cout<<"Inside State1\n";
-  if (!(robot->isLightBumperCenterRight()) && !(robot->isLightBumperFrontRight())) {
-    robot->drive(0.0, 0.0);
-  }
-  else {
-    while ((robot->isLightBumperFrontRight()) || (robot->isLightBumperCenterRight())) {
-      robot->drive(0.0,-1.0);
+  //If its only the far right bumper that is on, move straight
+  state[previous] = 1;
+    while ((robot->isLightBumperFrontRight()) || (robot->isLightBumperCenterRight())
+              || (robot->isLightBumperRight())) {
+      //Turn left
+      robot->drive(0.0,0.1);
     }
-    robot->drive(1.0,0.0);
-  }
+    //Move straight
+    robot->drive(0.1,0.0);
 }
-//ERROR IN WHILE LOOP!!!!!!!!
-//
-//BELOW!
-//
-//ERROR IN WHILE LOOP!!!!!!!!
 
+//If current state is two, then no light bumpers are currently on
 void goState2(create::Create* robot, int state[2]) {
   std::cout<<"Inside State2\n";
-  if (state[0] == 0) {
-    while (!(robot->isLightBumperLeft()) ||
-          !(robot->isLightBumperFrontLeft()) ||
-           !(robot->isLightBumperCenterLeft())) {
-             robot->drive(0.0,1.0);
-    }
-  }
-  if (state[0] == 1) {
-    while (!(robot->isLightBumperRight()) ||
-          !(robot->isLightBumperFrontRight()) ||
-           !(robot->isLightBumperCenterRight())) {
-             robot->drive(0.0,-1.0);
-    }
-  }
-  robot->drive(1.0,0.0);
+  //If previously left bumpers were on (ie state 0)
+  //Drive straight
+  robot->drive(0.1,0.0);
   return;
 }
-
+//If current straight is three, then its any combination of light bumpers that
+//is not state zero or one
 void goState3(create::Create* robot,int state[2]) {
   std::cout<<"Inside State3\n";
-
-  if (state[0] == 1) {
+  //If previously any right light bumpers were on (ie state 1)
+  if (state[previous] == 1) {
+    //Turn left until only light right bumpers are on again
     while ((robot->isLightBumperLeft()) ||
           (robot->isLightBumperFrontLeft()) ||
            (robot->isLightBumperCenterLeft())) {
-             robot->drive(0.0,1.0);
+             //Turn left
+             robot->drive(0.0,0.1);
     }
   }
-  if (state[0] == 0) {
+  //If previously any left bumpers were on (ie state )
+  if (state[previous] == 0) {
+    //Turn right until only left light bumpers are on again
     while ((robot->isLightBumperRight()) ||
           (robot->isLightBumperFrontRight()) ||
            (robot->isLightBumperCenterRight())) {
-             robot->drive(0.0,-1.0);
+             //Turn Right
+             robot->drive(0.0,-0.1);
     }
   }
-  robot->drive(1.0,0.0);
+  //Move straight
+  robot->drive(0.1,0.0);
 }
 
+//Get current state
 int getState(create::Create* robot) {
   std::cout<<"Inside getState\n";
+  //If any left bumpers and no right bumpers, its in state 0
   if (((robot->isLightBumperLeft()) ||
         (robot->isLightBumperFrontLeft()) ||
          (robot->isLightBumperCenterLeft())) &&
@@ -148,7 +158,9 @@ int getState(create::Create* robot) {
                 !(robot->isLightBumperCenterRight()))) {
                   std::cout<<"getState returns 0\n";
                   return 0;
+
   }
+//If any right bumpers and no left bumpers, its in state 1
   if ((!(robot->isLightBumperLeft()) &&
         !(robot->isLightBumperFrontLeft()) &&
          !(robot->isLightBumperCenterLeft())) &&
@@ -158,6 +170,7 @@ int getState(create::Create* robot) {
                   std::cout<<"getState returns 1\n";
                   return 1;
   }
+//If not bumpers its in state 2
   if (!(robot->isLightBumperLeft()) &&
         !(robot->isLightBumperFrontLeft()) &&
          !(robot->isLightBumperCenterLeft()) &&
@@ -167,12 +180,13 @@ int getState(create::Create* robot) {
                   std::cout<<"getState returns 2\n";
                   return 2;
   }
+  //Any other bumper combination, its in state 3
   else {
     std::cout<<"getState returns 3\n";
     return 3;
   }
 }
-
+//Write x,y, and angle data to output file.
 void writeData(std::ofstream *dataFile){
   dataFile->open("mappingData.txt");
   *dataFile << robot->getPose().x << " "
