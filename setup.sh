7b8bb769b6a79764b2e10e681b942f1d92c26365
jbubel <justin.bubel@ttu.edu>
2016-12-10 05:05:39 -0600

    Fixed breezycreate2 install and subroutine calls

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 sudo sh WebClient/breezyInstall.sh
+sudo mv WebClient/BreezyCreate2/breezycreate2/ WebClient/
 sudo sh WebClient/libcreateInstall.sh
-sudo cp WebClient/__init__.py WebClient/BreezyCreate2/
+sudo cp WebClient/__init__.py WebClient/subroutines/
+sudo cp WebClient/__init__.py WebClient/subroutines/python/
