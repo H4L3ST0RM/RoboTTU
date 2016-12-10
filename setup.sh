46ea2586e3991e93a26ec41ca928139e483f0326
jbubel <justin.bubel@ttu.edu>
2016-12-10 07:09:28 -0600

    Actually fixed breezycreate2 install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo sh WebClient/breezyInstall.sh
 sudo mv WebClient/BreezyCreate2/breezycreate2/ WebClient/
-sudo cp WebClient/breezycreate2/ WebClient/mobile/
+sudo cp -r WebClient/breezycreate2/ WebClient/mobile/
 sudo sh WebClient/libcreateInstall.sh
 sudo cp WebClient/__init__.py WebClient/subroutines/
 sudo cp WebClient/__init__.py WebClient/subroutines/python/
